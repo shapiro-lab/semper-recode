f7dbf269e6bfabb9bb6eb034f1e053b44cabd6a4
sujaritchai, gayvalin <tammygayvalin@gmail.com>
2023-06-29 22:00:49 -0700

    path argument add to class constructor, useless functions removed

--- semper_recode/semper_recode.py
+++ semper_recode/semper_recode.py
@@ -19,7 +19,7 @@ from Bio import SeqIO
 # import ishaan_utils
 
 class SemperRecode:
-    def __init__(self, user_seq = None):
+    def __init__(self, user_seq = None, path = "../data/"):
         """
         Initializes the SemperRecode object.
 
@@ -27,30 +27,23 @@ class SemperRecode:
         ----------
         user_seq : str, optional
             User-defined sequence, by default None. In case user want to tune single sequence instead of importing a whole FASTA file
-
-        Returns
-        -------
-        None
-
-        Raises
-        ------
-        None
+        path : str
+            Path to user's data which will be use to append to file names when file path is required
         """
-        self.pwm_df = None
-        self.aa_chart = None
-        self.codon_usage = None
-        self.start_codon = None
-        self.codon_dict = None
-        self.four_letter_codes = None
+        self.start_codon = ['AUG', 'ATG', 'GUG', 'UUG'] # Use in find_in_frame()
+        self.four_letter_codes = None # Use in filtered_sequence()
         self.master_df = None
-        self.start_codon = None
-        self.load_data()
-        
+        self.load_data(path)
         self.seq = Seq(user_seq) if user_seq is not None else 'ATGCTGACGGTAUGGACTTACCTGTATGCGTGCTAAATGCTAAGGCTGGTGCCGACCGGACCGTTGGGAGCGCTGTTGACCGGATGCTAAAGGGCCCGAGTCTTGTAGTACCGGACTTAAATGCGTTGTTTGACACCTGTT'
-
-    def load_data(self):
+    
+    def load_data(self, path):
         """
         Loads data required for the analysis. Reads various data files and initializes necessary attributes.
+        
+        Parameters
+        ----------
+        path : str
+            User-defined sequence, by default None. In case user want to tune single sequence instead of importing a whole FASTA file
 
         Returns
         -------
@@ -62,28 +55,12 @@ class SemperRecode:
             If any of the required data files are not found.
 
         """
-        # Position weight matrix
-        self.pwm_df = pd.read_csv('../data/pwm_df.csv') # Position Weight Matrix
-
-        # Amino acids (full-name, 3-letter, 1-letter)
-        self.aa_chart = pd.read_csv('../data/aa_chart.csv')
-
-        # Codon chart
-        self.codon_usage = pd.read_csv('../data/codon_usage.csv')
-
-        # List of start codons
-        self.start_codon = ['AUG', 'ATG', 'GUG', 'UUG']
-
         # Master dataframe
-        self.master_df = pd.read_csv('../data/master_df_os_2023.csv')
+        self.master_df = pd.read_csv(path + 'master_df_os_2023.csv')
         
         # Four letters codes
-        with open('../data/four_letter_codes.pkl', 'rb') as file:
+        with open(path + 'four_letter_codes.pkl', 'rb') as file:
             self.four_letter_codes = pickle.load(file)
-
-        # Dictionary of all codon that produce the same amino acid ex: {'A' : {'GCC', 'GCT', 'GCA', 'GCG'}}
-        with open('../data/codon_dict.pkl', 'rb') as file:
-            self.codon_dict = pickle.load(file)
 
     def process_sequence(self, file_path):
         """
@@ -111,6 +88,10 @@ class SemperRecode:
             for record in SeqIO.parse(file, 'fasta'):
                 sequence = str(record.seq)
                 modified_sequences.append(self.modify_TIS(sequence))
+
+        export_file_name = input("Enter the name of export file")
+        self.to_fasta(self, modified_sequences, export_file_name)   
+
         return modified_sequences
 
     def modify_TIS(self, sequence):
@@ -183,94 +164,6 @@ class SemperRecode:
         '''
         with open(output_file_name, 'w') as file:
             SeqIO.write(output, file, 'fasta')
-
-    def get_nuc_TIS_combos(self, aa_combo, pwm_samples_df):
-        """
-        Takes in a four-character string representative of amino acids and returns a list of the possible
-        nucleotide sequences from -6 to +5 with all possible degenerate codons used along with the predicted
-        efficiency from the PWM.
-
-        Parameters
-        ----------
-        aa_combo : str
-            A four-character string representing amino acids.
-        pwm_samples_df : pd.DataFrame
-            A DataFrame containing the PWM samples.
-
-        Returns
-        -------
-        pd.DataFrame
-            A DataFrame merging the combinations of nucleotide sequences with the PWM samples.
-        
-        """
-
-        aa_list_0 = list(self.codon_dict[aa_combo[0]].keys())
-        aa_list_1 = list(self.codon_dict[aa_combo[1]].keys())
-        aa_list_2 = list(self.codon_dict[aa_combo[2]].keys())
-        aa_list_3 = list(self.codon_dict[aa_combo[3]].keys())
-
-        aa_lists = [aa_list_0, aa_list_1, aa_list_2, aa_list_3]
-
-        # removes one nucleotide at +6 position
-        combinations = [''.join(p) for p in itertools.product(*aa_lists)]
-        tis_combinations = [''.join(p)[0:-1] for p in itertools.product(*aa_lists)]
-
-        combo_df = pd.DataFrame({'4-codons':combinations, '4-letters':aa_combo, 'tis-sequence': tis_combinations})
-
-        return combo_df.merge(pwm_samples_df, how='left', left_on = 'tis-sequence', right_on = 'sequence')
-
-    def generate_aa_combinations(self):
-        """
-        Generate all possible amino acid combinations and generate a list of unique four-letter codes.
-        The combinations are formed by appending the amino acid sequence with 'M' as the 3rd character.
-        After generating, the method retrieves nucleotide TIS combinations for each four-letter code
-        using the `get_nuc_TIS_combos` method.
-
-        Parameters
-        ----------
-        None
-
-        Raises
-        ------
-        ValueError
-            If duplicates are found in the generated list of amino acid combinations.
-
-        Returns
-        -------
-        List of four-letter codes from master_df
-
-        Examples
-        --------
-        >>> obj = MyClass()
-        >>> obj.explore_aa_combinations()
-
-        See Also
-        --------
-        get_nuc_TIS_combos : Get nucleotide TIS (Translation Initiation Site) combinations.
-        pd.DataFrame : pandas DataFrame object for data manipulation.
-
-        """
-        aa_list = list(self.aa_chart['1-letter'].unique())  # Get unique 1-letter amino acid codes
-        aa_TIS_list = []  # Initialize list for amino acid combinations with 'M'
-
-        for combo in product(aa_list, repeat=3):  # Generate all combinations of 3 amino acids
-            aa_TIS_list.append(combo[0] + combo[1] + 'M' + combo[2])  # Append combination with 'M'
-
-        dup = pd.Series(aa_TIS_list).duplicated()  # Check for duplicates in the combination list
-
-        if dup.any():  # If duplicates are found
-            raise ValueError("Duplicates found in the list!")
-
-        master_df = pd.DataFrame(columns=['4-codons', '4-letters'])  # Initialize a master DataFrame
-
-        for item in aa_TIS_list:  # Iterate over each amino acid combination
-            df = self.get_nuc_TIS_combos(item, self.pwm_df)  # Get nucleotide TIS combos for the combination
-            master_df = pd.concat([master_df, df])  # Concatenate the resulting DataFrame with master_df
-
-        master_df = master_df.sort_values(by=['efficiency'], ascending=True).drop_duplicates(keep='first')  # Sort and drop duplicates in master_df
-        master_df = master_df.reset_index(drop=True)  # Reset index of master_df
-
-        return list(master_df['4-letters'].unique())  # Get unique four-letter codes from master_df
 
     def filtered_sequence(self, efficiency):
         """
