20305adeda026610f252b81a4ad5f3a8ad0b4ccc
sujaritchai, gayvalin <tammygayvalin@gmail.com>
2023-06-29 20:51:13 -0700

    Basic function + docstrings implemented

--- semper_recode/semper_recode.py
+++ semper_recode/semper_recode.py
@@ -9,48 +9,114 @@ import pandas as pd
 import numpy as np
 import re
 from collections import Counter
-# import seaborn as sns
-# import matplotlib.pyplot as plt
+import pickle
 import itertools
-from itertools import combinations_with_replacement
 from itertools import product
 from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+
 # import ishaan_utils
 
-class BioSeq:
-    def __init__(self):
-        self.four_letter_codes = []
-        self.pwm_df, self.aa_chart, self.codon_usage, self.codon_dict = self.load_data()
+class SemperCode:
+    def __init__(self, user_seq = None):
+        self.pwm_df, self.aa_chart, self.codon_usage, self.start_codon, self.codon_dict, self.four_letter_codes, self.master_df = None, None, None, None, None, None, None
+        self.load_data()
+        self.start_codon = ['AUG', 'ATG', 'GUG', 'UUG']
+        self.seq = Seq(user_seq) if user_seq is not None else 'ATGCTGACGGTAUGGACTTACCTGTATGCGTGCTAAATGCTAAGGCTGGTGCCGACCGGACCGTTGGGAGCGCTGTTGACCGGATGCTAAAGGGCCCGAGTCTTGTAGTACCGGACTTAAATGCGTTGTTTGACACCTGTT'
 
     def load_data(self):
         # Position weight matrix
-        pwm_df = pd.read_csv('../data/noderer-dinuc-pwm.txt', delimiter='\t', skiprows=1)
-        pwm_df['sequence'] = pwm_df['sequence'].str.replace('U', 'T')
-        pwm_df = pwm_df.sort_values(by=['efficiency'], ascending=True)
-        pwm_df = pwm_df.reset_index(drop=True)
+        self.pwm_df = pd.read_csv('../data/pwm_df.csv') # Position Weight Matrix
 
         # Amino acids (full-name, 3-letter, 1-letter)
-        aa_chart = pd.read_csv('../data/amino-acid-codes.csv')
-        aa_chart['1-letter'] = np.where(aa_chart['1-letter'] == 'STOP', '*', aa_chart['1-letter'])
-        one_letter_codes = list(aa_chart['1-letter'].unique())
+        self.aa_chart = pd.read_csv('../data/aa_chart.csv')
 
-
-        # Codon chart (merge codon chart with aa_chart)
-        codon_usage = pd.read_csv('../data/nuclear_codon_statistics.tsv', delimiter='\t', skiprows=8)
-        codon_usage = codon_usage.merge(aa_chart, how='right', left_on='Amino acid', right_on='3-letter')
-        codon_usage = codon_usage.sort_values(by=['1-letter', 'Fraction'], ascending=[True, False])
-        codon_usage = codon_usage.reset_index(drop=True)
-
-        # Create dictionary of all codon that produce the same amino acid ex: {'A' : {'GCC', 'GCT', 'GCA', 'GCG'}}
-        codon_dict = {}
-        for aa in one_letter_codes:
-            codon_dict[aa] = dict(codon_usage.loc[codon_usage['1-letter'] == aa, ['CODON', 'Fraction']].values)
+        # Codon chart
+        self.codon_usage = pd.read_csv('../data/codon_usage.csv')
 
         # List of start codons
-        start_codon = ['AUG', 'ATG', 'GUG', 'UUG']
 
-        return pwm_df, aa_chart, codon_usage, codon_dict
+        # Master dataframe
+        self.master_df = pd.read_csv('../data/master_df_os_2023.csv')
+        
+        # Four letters codes
+        with open('../data/four_letter_codes.pkl', 'rb') as file:
+            self.four_letter_codes = pickle.load(file)
 
+        # Dictionary of all codon that produce the same amino acid ex: {'A' : {'GCC', 'GCT', 'GCA', 'GCG'}}
+        with open('../data/codon_dict.pkl', 'rb') as file:
+            self.codon_dict = pickle.load(file)
+
+    def process_sequence(self, file_path):
+        '''
+        Takes in fasta file path and return the modified sequence with lower efficiency (if possible)
+
+        Return:
+            Modiefied sequence (.fasta)
+        '''
+        modified_sequences = []
+
+        with open(file_path, 'r') as file:
+            for record in SeqIO.parse(file, 'fasta'):
+                sequence = str(record.seq)
+                modified_sequences.append(self.modify_TIS(sequence))
+        return modified_sequences
+
+    def modify_TIS(self, sequence):
+        '''
+        Takes in sequence (str) and return modified sequence with lower TIS efficiency
+
+        Arguments:
+            sequence (str)
+        Return:
+            modified_sequence (str)
+        '''
+
+    def find_in_frame(self, sequence):
+        '''
+        Takes in sequence (str) and return the list of index in which the AUG is found
+        '''
+        pos = []  # position(s) of in-frame AUG in the string (sequence)
+
+        # Loop through codon by codon (start from 0, end at last codon, increase by 3)
+        for i in range(0, len(sequence), 3):
+            codon = sequence[i:i+3]
+
+            if codon in self.start_codon:
+                pos.append(i)
+
+        return pos
+
+    def to_fasta(self, sequence, output_file_name):
+        '''
+        Takes in a list of modified sequence and convert them back to fasta to export to users
+        
+        Arguments:
+            Sequence (list) : modified list we wish to convert to fasta file
+        
+        Return:
+            Fasta file (.fasta)
+
+        '''
+        output = []
+
+        for i, seq in enumerate(sequence):
+            temp = SeqRecord(Seq(seq), id=f'Seq{i+1}', description='')
+            output.append(temp)
+        '''
+        i = index as wel iterated through sequence 
+
+        In each iteration, a new SeqRecord object (Seq object but with additional metadata)
+        is being created 
+
+        Metadata:
+            id: index+1
+            description: None
+1
+        '''
+        with open(output_file_name, 'w') as file:
+            SeqIO.write(output, file, 'fasta')
 
     def get_nuc_TIS_combos(self, aa_combo, pwm_samples_df):
         """
@@ -58,13 +124,17 @@ class BioSeq:
         nucleotide sequences from -6 to +5 with all possible degenerate codons used along with the predicted
         efficiency from the PWM.
 
-        Args:
-            aa_combo (str): A four-character string representing amino acids.
-            pwm_samples_df (pd.DataFrame): A DataFrame containing the PWM samples.
+        Parameters
+        ----------
+        aa_combo : str
+            A four-character string representing amino acids.
+        pwm_samples_df : pd.DataFrame
+            A DataFrame containing the PWM samples.
 
-        Returns:
-            pd.DataFrame: A DataFrame merging the combinations of nucleotide sequences with the PWM samples.
-
+        Returns
+        -------
+        pd.DataFrame
+            A DataFrame merging the combinations of nucleotide sequences with the PWM samples.
         """
 
         aa_list_0 = list(self.codon_dict[aa_combo[0]].keys())
@@ -87,24 +157,31 @@ class BioSeq:
         Generate all possible amino acid combinations and generate a list of unique four-letter codes.
         The combinations are formed by appending the amino acid sequence with 'M' as the 3rd character.
         After generating, the method retrieves nucleotide TIS combinations for each four-letter code
-        using the `get_nuc_TIS_combos` method
+        using the `get_nuc_TIS_combos` method.
 
-        Args:
-            None
+        Parameters
+        ----------
+        None
 
-        Raises:
-            ValueError: If duplicates are found in the generated list of amino acids combinations.
+        Raises
+        ------
+        ValueError
+            If duplicates are found in the generated list of amino acid combinations.
 
-        Returns:
-            None
+        Returns
+        -------
+        List of four-letter codes from master_df
 
-        Examples:
-            >>> obj = MyClass()
-            >>> obj.explore_aa_combinations(efficiency_threshold=40)
+        Examples
+        --------
+        >>> obj = MyClass()
+        >>> obj.explore_aa_combinations()
 
-        See Also:
-            - :func:`get_nuc_TIS_combos`: Get nucleotide TIS (Translation Initiation Site) combinations.
-            - :class:`pd.DataFrame`: pandas DataFrame object for data manipulation.
+        See Also
+        --------
+        get_nuc_TIS_combos : Get nucleotide TIS (Translation Initiation Site) combinations.
+        pd.DataFrame : pandas DataFrame object for data manipulation.
+
         """
         aa_list = list(self.aa_chart['1-letter'].unique())  # Get unique 1-letter amino acid codes
         aa_TIS_list = []  # Initialize list for amino acid combinations with 'M'
@@ -126,20 +203,26 @@ class BioSeq:
         master_df = master_df.sort_values(by=['efficiency'], ascending=True).drop_duplicates(keep='first')  # Sort and drop duplicates in master_df
         master_df = master_df.reset_index(drop=True)  # Reset index of master_df
 
-        self.four_letter_codes = list(master_df['4-letters'].unique())  # Get unique four-letter codes from master_df
+        return list(master_df['4-letters'].unique())  # Get unique four-letter codes from master_df
 
-    def troublesome_sequence(self, efficiency, df):
+    def filtered_sequence(self, efficiency):
         """
-        Takes in efficiency level input and finds the sequences in df (dataframe) by filtering, then displays all the possible sequences that have an efficiency lower than the given value and the percentage of found and missing amino acid sequences in the form of a dataframe.
+        Takes in efficiency level input and finds the sequences in the dataframe by filtering,
+        then displays all the possible sequences that have an efficiency lower than the given value
+        and the percentage of found and missing amino acid sequences in the form of a dataframe.
 
-        Args:
-            efficiency (float): The efficiency level to filter the dataframe.
-            df (pd.DataFrame): The dataframe containing the sequences and efficiencies.
+        Parameters
+        ----------
+        efficiency : float
+            The efficiency level to filter the dataframe.
 
-        Returns:
-            pd.DataFrame: A dataframe containing the filtered sequences.
-
+        Returns
+        -------
+        pd.DataFrame
+            A dataframe containing the filtered sequences.
         """
+        df = self.master_df
+
         filtered = df[df['efficiency'] < efficiency]
         count = filtered.shape[0]
         percent = (count / df.shape[0]) * 100
@@ -162,4 +245,29 @@ class BioSeq:
             print(each, "\n")
 
         return filtered.DataFrame
+    
+    def find_lower_eff_sequence(self, efficiency, seq):
+        """
+        Takes in a nucleotide sequence and checks if there is another sequence that produces
+        the same protein but with a lower efficiency level than the input level.
+
+        Parameters
+        ----------
+        efficiency : float
+            The efficiency level to filter the dataframe.
+        seq : Bio.Seq
+            nucleotide sequence
+
+        Raises
+        ------
+
+
+        Returns
+        -------
+        Bio.Seq
+            Nucleotide sequence with lower efficiency level
+        """
+
+        possible_seq_df = self.filtered_sequence(efficiency, self.master_df)
+
 
